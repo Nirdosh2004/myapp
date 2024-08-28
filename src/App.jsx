030704b9acf77a0417189e66475eec95d9093894
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-08-28 18:01:08 +0530

    1hr05minutes42seconds

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import {MyHeading,MyHeading1 as MeraName} from "./Components/MyHeading"
+import React from 'react'
+import Header from './Components/Header'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './Components/Home'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Product from './Components/Product'
 
 
-
-function App(){
-
-  
-
-
+const App = () => {
   return (
-    <>
-    <MeraName/>
-    <MyHeading name="Nirdosh" text="nice one" price="33232"/>
-    <p>hello </p>
-    </>
+   <Router>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/About' element={<About />} />
+      <Route path='/Contact/:id' element={<Product/>} />
+      <Route path='*' element={
+        <div>Page not found 404</div>
+      } />
+    </Routes>
+   </Router>
   )
 }
 
---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/Header.css"
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+  return (
+   <nav>      
+          <Link to="/">Home</Link>
+          <Link to="/About">About</Link>
+          <Link to="/Contact">Contact</Link>
+   </nav>
+  )
+}
+
+export default Header
